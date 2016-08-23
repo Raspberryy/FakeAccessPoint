81db4ee71f993b281d27fb8dbc979fb39ea0aec2
Raspberryy <Raspberryy@users.noreply.github.com>
2016-08-23 17:53:40 +0200

    Update FakeAP.py

--- FakeAP.py
+++ FakeAP.py
@@ -182,6 +182,7 @@ def StartEttercap():
  
 def SetIpRules(Gateway, InterfaceInternet):
 	print yellow + "[*]"  + white + "Routing Rules"
+	time.sleep(5)
 	os.system("ifconfig at0 192.168.1.1 netmask 255.255.255.0")
 	os.system("ifconfig at0 mtu 1400")
 	os.system("route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1")
@@ -204,4 +205,5 @@ def HelpFunction():
 # Main Program
 
 GettingAttributes()
+os.system(airmon-ng stop mon0)
 
