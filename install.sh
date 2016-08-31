ec4b7ca8f430f8752ddbd78a4691aaff56451562
Raspberryy <Raspberryy@users.noreply.github.com>
2016-08-31 15:45:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ rm ez_setup.py
 rm setuptools-26.0.0.zip
 
 # DHCP3 Server
-sudo apt-get install isc-dhcp-server --allow
+sudo apt-get install isc-dhcp-server -y --force-yes
 
 if [ "$1" != "--kali" ]
 then
